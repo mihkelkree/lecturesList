32d064aa2dc9cc9e460d5c2b91df8b01f51a1bfd
Mihkel Kree <mihkel.kree@gmail.com>
2017-03-08 21:57:04 +0200

    added tabletop to read google drive

--- myScript.js
+++ myScript.js
@@ -1,43 +1,33 @@
 
+url = 'https://docs.google.com/spreadsheets/d/1nK4XywW8SrsP2IkRaImwXg7BT_W_sNGDxtyrHJTesIU/pubhtml';
 
-url = 'https://docs.google.com/spreadsheets/d/1nK4XywW8SrsP2IkRaImwXg7BT_W_sNGDxtyrHJTesIU/pub?gid=0&single=true&output=csv';
 
-// manually made to match with the ones on the Google Drive table
-var groupNames = ["Loengud", "Programmid"];
-var allData = [ [], []];
+callTabletop = function () {
+    Tabletop.init( { key: url,
+                     callback: showInfo,
+                     simpleSheet: true } );
+}
 
-callLoad = function(){
-
-$.get(url, function(data) { 
-	s=data;
-	s=s.split('\n');
+function showInfo(mydata, tabletop){
 	
-	console.log(s);
+	//console.log(tabletop.foundSheetNames);
 	
-	var groupID=0;
+	// generate DOMs for lectures
+	var activeData = tabletop.sheets("Loengud").all();
+	//console.log(lecData);
+	var lectures=d3.select("#lectures");	
+	lectures.selectAll("div").data(activeData).enter().append("div").attr("class","entry").
+		append('p').attr("class","lecname").text(function(d){ return d["Lektor"]}).
+		append('p').attr("class","title").text(function(d){ return d["Pealkiri"]}).
+		append('p').attr("class","content").text(function(d){ return d["Kirjeldus"]});
 
-	// read and parse the data
-	for (i=0; i<s.length; i++){
-		a=s[i];
-		a=a.split(',');
+
+	// generate DOMs for programs
+	activeData = tabletop.sheets("Programmid").all();	
+	var programs=d3.select("#programs");	
+	programs.selectAll("div").data(activeData).enter().append("div").attr("class","entry").
+		append('p').attr("class","lecname").text(function(d){ return d["Lektor"]}).
+		append('p').attr("class","title").text(function(d){ return d["Pealkiri"]}).
+		append('p').attr("class","content").text(function(d){ return d["Kirjeldus"]});
 		
-		if (a[0]=="" || a[0]=="Lektor"){continue;}
-		if (a[1]==""){
-			//console.log(a[0])
-			switch(a[0]){
-				case groupNames[0]: groupID=0; break;
-				case groupNames[1]: groupID=1; break;
-				default: groupID=0; break;
-			}
-			continue;
-		}
-		
-		d={lektor:a[0], pealkiri:a[1], kirjeldus:a[2]};
-		allData[groupID].push(d);
-	}
-	
-	console.log(allData);
-	
-});
-
 }
