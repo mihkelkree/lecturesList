1366377f02721ca81c4bcab68441351daddf8902
Mihkel Kree <mihkel.kree@gmail.com>
2017-08-04 14:21:35 +0300

    some edits

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This code is for automatically generating a long list of lecture annotations based on data entries in a Google Drive spreadsheet. The displayed lectures can be filtered.
 
 See the [demo](http://kodu.ut.ee/~mihkel84/loengutedemo/).
+
+The code to be copy-pasted into Drupal page entry is in the file UT_lehele.html. Lots of inline css (mostly margins) has to be used for correct formatting.
--- js/myScript.js
+++ js/myScript.js
@@ -55,67 +55,12 @@ function showInfo(mydata, tabletop){
 	
 	d3.selectAll('#wait').remove();
 	
-	//console.log(tabletop.foundSheetNames);
-	
 	// generate DOMs for lectures
 	var loengudData = tabletop.sheets("Loengud").all();
 	
 	generateEntries(loengudData);
 	return;
 	
-	//console.log(lecData);
-	var lectures=d3.select("#lectures");	
-	
-	// lecture entry
-	lectures = lectures.selectAll("div").data(activeData).enter().append("div").attr("class","entry");
-	
-	var shortrow = lectures.append('div').attr("class","shortrow"); 
-	shortrow.append('p').attr("class","shortname").
-		text(function(d,i){return "<b>"+d["Lektor"]+"</b>, "+d["Pealkiri"]}).
-		on('click', activate);
-	
-	var expandable = lectures.append('div').attr("class","expandbox").
-		style('display','none'); 
-	
-	
-	// picture div floating left
-	expandable.append('div').attr("class","pic").
-		style('display', function(d){ if (d["Foto"]) return "inline"; else return "none"}).
-		append('img').attr('src',function(d){ return d["Foto"]}).attr('width',picWidth);
-			
-	
-	// info div
-	var info = expandable.append('div').attr("class","info");
-	
-	// header row
-	var lecrow = info.append('div').attr("class","lechead");
-	
-	// faclulty and age group
-	var infodiv = lecrow.append('div').attr("class","lecinfodiv");
-	infodiv.append('p').attr("class","faculty").text(function(d){return "valdkond: "+d["Valdkond"]});
-	infodiv.append('p').attr("class","agegroup").text(function(d){return "sihtrühm: "+d["Kooliaste"]});
-	
-	// lector name and title
-	var namediv = lecrow.append('div').attr("class","lecnamediv");
-	namediv.append('p').attr("class","lectorname").text(function(d){ return d["Lektor"]});
-	namediv.append('p').attr("class","lectitle").text(function(d){ return d["Pealkiri"]});
-	
-	// descriotion, info, etc
-	info.append('p').attr("class","leccontent").text(function(d){ return d["Kirjeldus"]});
-	info.append('p').attr("class","leccontact").text(function(d){ return d["Kontakt"]});
-	info.append('p').attr("class","lecextrainfo").
-		text(function(d){ if (d["Lisainfo"]) return "Lisainfo: "+d["Lisainfo"]});
-	
-
-
-	// generate DOMs for programs
-	activeData = tabletop.sheets("Programmid").all();	
-	var programs=d3.select("#programs");	
-	programs.selectAll("div").data(activeData).enter().append("div").attr("class","entry").
-		append('p').attr("class","lecname").text(function(d){ return d["Lektor"]}).
-		append('p').attr("class","title").text(function(d){ return d["Pealkiri"]}).
-		append('p').attr("class","content").text(function(d){ return d["Kirjeldus"]});
-		
 }
 
 function generateEntries(data){
@@ -198,11 +143,11 @@ function generateEntries(data){
 	infodiv.append('p').attr("class","agegroup").
 		text(function(d){return "sihtrühm: "+d["Tase"]});
 	infodiv.append('p').attr("class","topic").
-		text(function(d){return "teema: "+d["Teema"]});
+		text(function(d){return ""+d["Teema"]});
 	
 	// lector name and title
 	var namediv = lecrow.append('div').attr("class","lecnamediv");
-	namediv.append('p').attr("class","lectorname").text(function(d){ return d["Lektor"]});
+	namediv.append('p').attr("class","lectorname").style("margin-bottom","0.3em").text(function(d){ return d["Lektor"]});
 	namediv.append('p').attr("class","lectitle").text(function(d){ return d["Pealkiri"]});
 	
 	// descriotion, info, etc
